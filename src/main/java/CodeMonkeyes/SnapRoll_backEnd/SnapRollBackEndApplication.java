d2bee4ab9a3a4cd3abaae9629f7ce85ad002000f
rajpatel923 <rajpatel123370@gmail.com>
2024-03-29 16:55:29 -0500

    Initial commit

---
+++ src/main/java/CodeMonkeyes/SnapRoll_backEnd/SnapRollBackEndApplication.java
@@ -0,0 +1,13 @@
+package CodeMonkeyes.SnapRoll_backEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapRollBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapRollBackEndApplication.class, args);
+	}
+
+}
